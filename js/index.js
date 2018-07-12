a20cbe24906b7f221df3b8692fa871f5e84b2146
shashi0802 <shashikant.yadav@contentstack.com>
2018-07-12 18:06:39 +0530

    first UI

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var marginY = 0;
+var destination = 0;
+var speed = 5;
+var scroller = null;
+
+function initScroll(elementId){
+
+    destination = document.getElementById(elementId).offsetTop;
+
+    scroller = setTimeout(function(){
+        initScroll(elementId);
+    },1);
+    marginY = marginY + speed;
+
+    if(marginY >= destination){
+        clearTimeout(scroller);
+    }
+
+    window.scroll(0, marginY);
+}
+
+console.log(document.getElementsByClassName('textbox').values);
